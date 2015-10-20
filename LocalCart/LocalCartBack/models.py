22214a5b650eedcadf665d579d8e6828fa099f69
Tom <tommeng@berkeley.edu>
2015-10-20 13:25:29 -0700

    setup the enviroment and test framework for backend

---
+++ LocalCart/LocalCartBack/models.py
@@ -0,0 +1,79 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+from django.contrib.auth.models import User
+
+class UserInfo(models.Model):
+    userID = models.ForeignKey(User) # use a one to one relationship?
+    user_type = models.CharField(max_length=16)
+    picture = models.CharField(max_length=128, null=True) # A url
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+class Store(models.Model):
+
+    # inventoryID = models.ForeignKey(Inventory)
+    userID = models.ForeignKey(User)
+    name = models.CharField(max_length=64)
+    address_street = models.CharField(max_length=64)
+    address_city = models.CharField(max_length=32)
+    address_state = models.CharField(max_length=32)
+    address_zip = models.CharField(max_length=16)
+    phone_number = models.CharField(max_length=16)
+    description = models.CharField(max_length=4096, null=True) # Allowed to be empty?
+    picture = models.CharField(max_length=128, null=True) # A url
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    # Add hours
+
+
+
+class Inventory(models.Model):
+
+    storeID = models.ForeignKey(Store)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+class Item(models.Model):
+
+    storeID = models.ForeignKey(Store)
+    inventoryID = models.ForeignKey(Inventory)
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=4096, null=True) # Allowed to be empty?
+    price = models.FloatField(max_length=4096)
+    picture = models.CharField(max_length=128, null=True) # A url
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Reviews(models.Model):
+
+    userID = models.ForeignKey(User)
+    storeID = models.ForeignKey(Store)
+    itemID = models.ForeignKey(Item)
+    rating = models.PositiveSmallIntegerField()
+    text = models.CharField(max_length=4096, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+class List(models.Model):
+
+    userID = models.ForeignKey(User)
+    name = models.CharField(max_length=64)
+
+
+
+class ListItem(models.Model):
+
+    listID = models.ForeignKey(List)
+    itemID = models.ForeignKey(Item, null=True)
+    item_name = models.CharField(max_length=64)
+    list_position = models.PositiveSmallIntegerField(unique=True)
+
---
+++ LocalCart/LocalCartBack/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^$', 'backend.views.home'),
+    url(r'^api/user/create$', 'backend.views.create_user'),
+    url(r'^api/user/login$', 'backend.views.log_in'),
+    url(r'^api/store/create$', 'backend.views.create_store'),
+    url(r'^api/inventory/create$', 'backend.views.create_inventory'),
+    url(r'^api/inventory/add$', 'backend.views.create_item'),
+    url(r'^api/item/create$', 'backend.views.create_item'),
+    url(r'^api/item/edit$', 'backend.views.edit_item'),
+]
+
+
