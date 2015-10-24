09a0806630286d22a50924db82afc8ce9944eb34
Tom <tommeng@berkeley.edu>
2015-10-23 19:26:46 -0700

    merging to master

--- testing/testBarely.py
+++ testing/testBarely.py
@@ -255,74 +255,7 @@ class TestCarts(testLib.CartTestCase):
                                              })    
 
         self.assertSuccessResponse(respSearchItem)                                      
-    #     # Now read the smiles
-    #     respGet = self.getSmiles(count='all')
-    #     self.assertSuccessResponse(respGet)
-    #     self.assertEquals(1, len(respGet['smiles']))
-    #     self.assertEquals(respCreate['smile']['id'], respGet['smiles'][0]['id'])
 
-    # def testAdd3(self):
-    #     """
-    #     Test adding three smile
-    #     """
-    #     respCreate1 = self.makeRequest("/api/smiles", method="POST",
-    #                                 data = { 'title' : 'A shy smile1',
-    #                                          'space' : self.smileSpace,
-    #                                          'story' : 'Once upon a time I was a shy boy...',
-    #                                          'happiness_level' : 1
-    #                                          })
-    #     respCreate2 = self.makeRequest("/api/smiles", method="POST",
-    #                                 data = { 'title' : 'A shy smile2',
-    #                                          'space' : self.smileSpace,
-    #                                          'story' : 'Once upon a time I was a shy boy...',
-    #                                          'happiness_level' : 2
-    #                                          })
-    #     respCreate3 = self.makeRequest("/api/smiles", method="POST",
-    #                                 data = { 'title' : 'A shy smile3',
-    #                                          'space' : self.smileSpace,
-    #                                          'story' : 'Once upon a time I was a shy boy...',
-    #                                          'happiness_level' : 3
-    #                                          })
-    #     self.assertSuccessResponse(respCreate1)
-    #     self.assertEquals(0, respCreate1['smile']['like_count'])
-    #     self.assertSuccessResponse(respCreate2)
-    #     self.assertEquals(0, respCreate2['smile']['like_count'])
-    #     self.assertSuccessResponse(respCreate3)
-    #     self.assertEquals(0, respCreate3['smile']['like_count'])
-    #     self.assertEquals('A shy smile1', respCreate1['smile']['title'])
-    #     self.assertEquals('A shy smile2', respCreate2['smile']['title'])
-    #     self.assertEquals('A shy smile3', respCreate3['smile']['title'])
-    #     self.assertEquals(1, respCreate1['smile']['happiness_level'])
-    #     self.assertEquals(2, respCreate2['smile']['happiness_level'])
-    #     self.assertEquals(3, respCreate3['smile']['happiness_level'])
-
-    #     respGet = self.getSmiles(count='all')
-    #     self.assertSuccessResponse(respGet)
-    #     self.assertEquals(3, len(respGet['smiles']))
-    #     # self.assertEquals(respCreate['smile']['id'], respGet['smiles'][0]['id'])
-
-    # # test for error when create new smile
-
-
-
-    
-    # def testIncrementLike(self):
-
-    #     respCreate = self.makeRequest("/api/smiles", method="POST",
-    #                                 data = { 'title' : 'A shy smile',
-    #                                 'space' : self.smileSpace,
-    #                                 'story' : 'Once upon a time I was a shy boy...',
-    #                                 'happiness_level' : 1
-    #                                 })
-    #     respGet = self.getSmiles(count='all')
-    #     self.assertSuccessResponse(respGet)
-    #     resp_id = respGet['smiles'][0]['id']
-    #     url = "/api/smiles/" + str(resp_id) + "/like/"
-    #     respInc = self.makeRequest(url, method="POST")
-
-    #     respGet = self.getSmiles(count='all')
-    #     self.assertSuccessResponse(respGet)
-    #     self.assertEquals(1, respGet['smiles'][0]['like_count'])
 
 
 
