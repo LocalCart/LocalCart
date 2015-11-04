0207ad6f3533585a020cc8b08f58c5a59de6cf6d
Nguyen <ndinh27@ubuntu.ubuntu-domain>
2015-11-03 16:35:42 -0800

    Heroku

--- settings.py
+++ settings.py
@@ -141,11 +141,11 @@ STATICFILES_DIRS = (
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
 
-# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# # Allow all host headers
-# ALLOWED_HOSTS = ['*']
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
 
 # # Static asset configuration
 # import os
