c853da2af87c762ec3fce673251dfb635b73a2fd
Linda Zhang <smartydumdum@airbears2-10-142-33-110.airbears2.1918.berkeley.edu>
2015-11-04 19:08:10 -0800

    Unit tests for create_user

--- testing/testUnitModels.py
+++ testing/testUnitModels.py
@@ -6,6 +6,29 @@ To test all tests: ./manage.py test
 To test specific file: ./manage.py test testing.file_name
 """
 
+class CustomerMocker:
+
+	def create_customer_user_Linda(self):
+		# Creates customer Linda.
+		username = "Linda"
+		password = "123"
+		email = "funfunfunfunfunfunfun@gmail.com"
+		user_type = "customer"
+		picture = 'images/default_user_image'
+		new_user_info = models.UserInfo.create_new_user(username=username, password=password, email=email, 
+			first_name='', last_name='', user_type=user_type, picture=picture)
+		return new_user_info
+
+	def create_customer_user_Ruth(self):
+		# Creates customer Linda.
+		username = "Ruth"
+		password = "123"
+		email = "rlzhang@berkeley.edu"
+		user_type = "customer"
+		picture = 'images/default_user_image'
+		new_user_info = models.UserInfo.create_new_user(username=username, password=password, email=email, 
+			first_name='', last_name='', user_type=user_type, picture=picture)
+		return new_user_info
 
 class UserInfoTestCase(TestCase):
 
@@ -16,7 +39,7 @@ class UserInfoTestCase(TestCase):
 		email = "rlzhang@berkeley.edu"
 		user_type = "customer"
 		picture = 'images/default_user_image'
-		new_user_info = models.create_new_user(username=username, password=password, email=email, 
+		new_user_info = models.UserInfo.create_new_user(username=username, password=password, email=email, 
 			first_name='', last_name='', user_type=user_type, picture=picture)
 		self.assertEqual(new_user_info.user.username, username)
 		self.assertEqual(new_user_info.user.email, email)
@@ -24,3 +47,23 @@ class UserInfoTestCase(TestCase):
 		self.assertEqual(new_user_info.user.last_name, '')
 		self.assertEqual(new_user_info.user_type, user_type)
 		self.assertEqual(new_user_info.picture, picture)
+
+	def test_create_two_same_users(self):
+		# Check that you cannot create two users of the same username.
+		customer = CustomerMocker()
+		original_user = customer.create_customer_user_Linda()
+		duplicate_user = customer.create_customer_user_Linda()
+		self.assertTrue(original_user)
+		self.assertTrue(duplicate_user is None)
+
+	def test_edit_user_info(self):
+		# Edit Linda's info
+		customer = CustomerMocker()
+		user = customer.create_customer_user_Linda()
+		first_name = "Linda"
+		last_name = "Zhang"
+		email = "r.linda.zhang@gmail.com"
+		password = "987"
+		picture = 'prettybirdies'
+		edited_user_type = models.UserInfo.edit_user_info(username=user.user.username)
+		self.assertEqual(edited_user_type, user.user_type)
